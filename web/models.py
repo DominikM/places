651e65046afcef979752387b5671148fe4be9639
Dominik Martinez <dominikm@mit.edu>
2017-06-08 21:04:03 -0700

    CheckIn notes can also be blank

--- web/models.py
+++ web/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CheckIn(models.Model):
     date = models.DateField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
     place = models.ForeignKey('Place')
 
     def __str__(self):
