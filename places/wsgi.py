31aeba94b7564bc0f3cfec0f342f260d2fd6571e
Dominik Martinez <dominikm@mit.edu>
2017-06-09 00:40:12 -0700

    Got frontend mostly working for creating togos

--- places/wsgi.py
+++ places/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+ACTIVATE = '/home/dominikm/python-envs/django/bin/activate_this.py'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+"""
+with open(ACTIVATE) as file_:
+    exec(file_.read(), dict(__file__=ACTIVATE))
+"""
+sys.path.insert(0, BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
