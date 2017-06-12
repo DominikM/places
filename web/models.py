95bda4deac45ae97b9389069b821406e5b8581c8
Dominik Martinez <dominikm@mit.edu>
2017-06-12 10:07:24 -0700

    Also updated tests and models, missed in the last commit :P

--- web/models.py
+++ web/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class CheckIn(models.Model):
@@ -18,6 +19,7 @@ class Place(models.Model):
     notes = models.TextField(blank=True)
     place_id = models.CharField(max_length=100, null=True) # for google places api, may not be necessary
     togo = models.BooleanField(default=False)
+    user = models.ForeignKey(User)
 
     def __str__(self):
         return self.name
--- web/tests.py
+++ web/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
+import json
 
 # Create your tests here.
 
 class WebTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(
+            'ben_bitdiddle',
+            'ben@gmail.com',
+            '123abc'
+        )
 
     def test_create_place(self):
+        self.client.login(username='ben_bitdiddle', password='123abc')
         response = self.client.post('/create/place/', {
             'name': 'Happy House',
             'address': '123 Happy Lane',
@@ -18,9 +26,9 @@ class WebTest(TestCase):
             'date': '2017-06-08',
             'checkin_notes': 'Good visit!'
         })
-        print(response.json()['place'])
+        print(response.json()['place']['fields'])
         self.assertEqual(response.json()['status'], 'OK')
-        self.assertJSONEqual(response.json()['place'],
+        self.assertDictEqual(response.json()['place']['fields'],
                          {
                              'name': 'Happy House',
                              'address': '123 Happy Lane',
@@ -28,4 +36,7 @@ class WebTest(TestCase):
                              'lng': 122.4194,
                              'notes': 'I love this place!',
                              'place_id': '123abc',
+                             'user': self.user.id,
+                             'togo': False,
+                             'id': 1
                          })
